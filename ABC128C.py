edd716c73006f3ecac93150b7738ee3ba583eee9
sthr630-arcturus <s_izumi630y@yahoo.co.jp>
2023-11-12 21:48:26 +0900

    complete 128

--- ABC128C.py
+++ ABC128C.py
@@ -1,27 +1,39 @@
-n, m = map(int, input().split())
-connect_switch = [[] for _ in range(m)]
-on_switch = [[] for _ in range(m)]
-for i in range(m):
-    connect_switch[i] = list(map(int, input().split()[1:]))
-    on_switch[i] = connect_switch[i]
+import numpy as np
 
-condition = list(map(int, input().split()))
+n, m = map(int, input().split())
+connect_switch = np.full((m, n), 0)
+light = np.full((m, n+2), 0)
+
+for i in range(m):
+    for j, sw_num in enumerate(list(map(int, input().split()[1:]))):
+        connect_switch[i][j] = sw_num
+
+for i, p in enumerate(list(map(int, input().split()))):
+    light[i][n+1] = p
+
+ans = 0
 
 for bit in range(1<<n):
-    is_switch_on = [False] * m
-    is_light_on = [False] * n
-    for i in range(m):
-        if 1 & (bit>>i):
-            is_switch_on[i] = True
+    is_on_switch = [False]*n
+    is_all_on = True
 
     for i in range(n):
-        for j in range(len(connect_switch[i])):
-            if is_switch_on[connect_switch[i][j]-1]:
-                on_switch[i][j] = 1
-            else:
-                on_switch[i][j] = 0
-    print(is_switch_on)
-    print(on_switch)
+        if 1 & bit >> i:
+            is_on_switch[i] = True
 
-#print(connect_switch)
-#print(condition)
+    is_on_switch.insert(0, False)
+
+    for i in range(m):
+        for j in range(n):
+            light[i][j] = is_on_switch[connect_switch[i][j]]
+
+    for i in range(m):
+        light[i][n] = sum(light[i][:n])%2
+
+    for i in range(m):
+        if not light[i][n] == light[i][n+1]:
+            is_all_on = False
+
+    if is_all_on:
+        ans += 1
+print(ans)
